fe292f4d40277306801c99d370863a1737e52cb1
harmanparihar <harmanparihar18@gmail.com>
2019-01-29 21:51:13 -0800

    mlab connectivity established

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = express();
-app.listen(3000,function(){
-  console.log('listening on 3000')
+const bodyParser=require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://circlelink:circle123@ds149344.mlab.com:49344/circledb',(err,client)=>{
+  if (err) return console.log(err)
+  db = client.db('circledb')
+  app.listen(3000,function(){
+    console.log('listening on 3000')
+  })
+})
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/index.html')
+})
+app.post('/qoutes',(req,res)=>{
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
